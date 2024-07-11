78690d8853eb9d90d973703b68e971c502fe3b3f
Prashant Kumar <prashant.aman9@gmail.com>
2024-07-11 17:14:51 +0530

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
-
-import React from "react";
-import './../styles/App.css';
+import React from 'react'
+import './../styles/App.css'
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+  const [name, setName] = React.useState('')
+
+  const handleChange = (e) => {
+    const { value } = e.target
+    setName(value)
+  }
+
+	return (
+		<div>
+			{/* Do not remove the main div */}
+			<label htmlFor='name'>Enter your name: </label>
+      <input type='text' id='name' name='name' value={name} onChange={handleChange} />
+      {name && <p>Hello {name}!</p>}
+		</div>
+	)
 }
 
 export default App
